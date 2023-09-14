22a6118eb8a3cf92f64212c3d1b0318fdd9a8096
Sumrankumar <121472963+Sumrankumar@users.noreply.github.com>
2023-09-14 19:40:23 +0530

    Add files via upload

---
+++ fees_script.js
@@ -0,0 +1,189 @@
+function getInputValue(){
+
+    let inputVal=document.getElementById("uid").value;
+    let a=1;
+
+    if(inputVal==1){
+        document.getElementById("demo").innerHTML ="AK";
+        document.getElementById("demo1").innerHTML ="10000";
+    }
+    else if(inputVal==2)
+    {
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";  
+    }
+    else if(inputVal==3)
+    {
+        document.getElementById("demo").innerHTML ="BK";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==4)
+    {
+        document.getElementById("demo").innerHTML ="CK";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==5)
+    {
+        document.getElementById("demo").innerHTML ="DK";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==6)
+    {
+        document.getElementById("demo").innerHTML ="EK";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==7)
+    {
+        document.getElementById("demo").innerHTML ="FK";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==8)
+    {
+        document.getElementById("demo").innerHTML ="GK";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+    else if(inputVal==9)
+    {
+        document.getElementById("demo").innerHTML ="HK";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==10)
+    {
+        document.getElementById("demo").innerHTML ="IK";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }
+
+     else if(inputVal==11)
+    {
+        document.getElementById("demo").innerHTML ="JK";
+        document.getElementById("demo1").innerHTML ="0";   
+    }
+     else if(inputVal==12)
+    {
+        document.getElementById("demo").innerHTML ="KK";
+        document.getElementById("demo1").innerHTML ="20000";   
+    }
+    else if(inputVal==13)
+    {
+        document.getElementById("demo").innerHTML ="LK";
+        document.getElementById("demo1").innerHTML ="7500";   
+    }  
+    else if(inputVal==14)
+    {
+        document.getElementById("demo").innerHTML ="MK";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==15)
+    {
+        document.getElementById("demo").innerHTML ="NK";
+        document.getElementById("demo1").innerHTML ="2500";   
+    }
+    else if(inputVal==16)
+    {
+        document.getElementById("demo").innerHTML ="OK";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+    else if(inputVal==17)
+    {
+        document.getElementById("demo").innerHTML ="PK";
+        document.getElementById("demo1").innerHTML ="10000";   
+    }
+
+    else if(inputVal==18)
+    {
+        document.getElementById("demo").innerHTML ="QK";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==19)
+    {
+        document.getElementById("demo").innerHTML ="RK";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==20)
+    {
+        document.getElementById("demo").innerHTML ="SK";
+        document.getElementById("demo1").innerHTML ="2500"; 
+    }
+     else if(inputVal==21)
+    {
+        document.getElementById("demo").innerHTML ="TK";
+        document.getElementById("demo1").innerHTML ="1000"; 
+    }
+    else if(inputVal==22)
+    {
+        document.getElementById("demo").innerHTML ="UK";
+        document.getElementById("demo1").innerHTML ="20000"; 
+    }
+    else if(inputVal==23)
+    {
+        document.getElementById("demo").innerHTML ="VK";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==24)
+    {
+        document.getElementById("demo").innerHTML ="WK";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==25)
+    {
+        document.getElementById("demo").innerHTML ="XK";
+        document.getElementById("demo1").innerHTML ="7500"; 
+    }
+    else if(inputVal==26)
+    {
+        document.getElementById("demo").innerHTML ="YK";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+     else if(inputVal==27)
+    {
+        document.getElementById("demo").innerHTML ="ZK";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==28)
+    {
+        document.getElementById("demo").innerHTML ="ABC";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+
+    else if(inputVal==29)
+    {
+        document.getElementById("demo").innerHTML ="DEF";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+     else if(inputVal==30)
+    {
+        document.getElementById("demo").innerHTML ="GHI";
+        document.getElementById("demo1").innerHTML ="0"; 
+    }
+    else if(inputVal==31)
+    {
+        document.getElementById("demo").innerHTML ="JKL";
+        document.getElementById("demo1").innerHTML ="2500"; 
+    }
+    else if(inputVal==32)
+    {
+        document.getElementById("demo").innerHTML ="MNO";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else if(inputVal==33)
+    {
+        document.getElementById("demo").innerHTML ="PQR";
+        document.getElementById("demo1").innerHTML ="7500"; 
+    }
+    else if(inputVal==34)
+    {
+        document.getElementById("demo").innerHTML ="STU";
+        document.getElementById("demo1").innerHTML ="20000 + 2500 = 22500"; 
+    }
+    else if(inputVal==35)
+    {
+        document.getElementById("demo").innerHTML ="VWX";
+        document.getElementById("demo1").innerHTML ="10000"; 
+    }
+    else{
+        document.getElementById("demo").innerHTML ="No Records Found";
+        document.getElementById("demo1").innerHTML ="0";
+    }
+    
+}
